7fc87875c72dbd298baf033d1e1687099e8ffc08
maxime <bertrand@nelis.fr>
2017-09-21 17:41:40 +0200

    WiP: plugged angular with webpack

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,27 +7,27 @@ class IndexController extends Controller
 	public function indexAction()
 	{
         // Angular
-        $this->assets->addJs("node_modules/angular/angular.min.js");
-        $this->assets->addJs("node_modules/angular-animate/angular-animate.min.js");
-        $this->assets->addJs("node_modules/angular-aria/angular-aria.min.js");
-        $this->assets->addJs("node_modules/angular-slugify/angular-slugify.js");
+        // $this->assets->addJs("node_modules/angular/angular.min.js");
+        // $this->assets->addJs("node_modules/angular-animate/angular-animate.min.js");
+        // $this->assets->addJs("node_modules/angular-aria/angular-aria.min.js");
+        // $this->assets->addJs("node_modules/angular-slugify/angular-slugify.js");
 
-        $this->assets->addJs("dist/bundle.js");
+        $this->assets->addJs("bundle.js");
 
         // Material design lite
         $this->assets->addCss("node_modules/material-design-lite/material.min.css");
         $this->assets->addJs("node_modules/material-design-lite/material.min.js");
 
         $this->assets->addCss("node_modules/mdi/css/materialdesignicons.min.css");
-        // $this->assets->addCss("dist/css/main.css");
+        // $this->assets->addCss("css/main.css");
 
-        $this->assets->addJs("dist/js/angular/app.js");
-        $this->assets->addJs("dist/js/angular/controllers/index.js");
-        $this->assets->addJs("dist/js/angular/controllers/skills.js");
-        $this->assets->addJs("dist/js/angular/controllers/experience.js");
-        $this->assets->addJs("dist/js/angular/controllers/training.js");
-        $this->assets->addJs("dist/js/angular/controllers/projects.js");
-        $this->assets->addJs("dist/js/angular/controllers/about.js");
-        $this->assets->addJs("dist/js/angular/controllers/hobbies.js");
+        $this->assets->addJs("js/angular/app.js");
+        // $this->assets->addJs("js/angular/controllers/index.js");
+        // $this->assets->addJs("js/angular/controllers/skills.js");
+        // $this->assets->addJs("js/angular/controllers/experience.js");
+        // $this->assets->addJs("js/angular/controllers/training.js");
+        // $this->assets->addJs("js/angular/controllers/projects.js");
+        // $this->assets->addJs("js/angular/controllers/about.js");
+        // $this->assets->addJs("js/angular/controllers/hobbies.js");
 	}
 }
--- public/src/js/angular/app.js
+++ public/src/js/angular/app.js
@@ -1,3 +1,7 @@
+const angular = require('angular');
+
+require('angular-slugify');
+
 angular
     .module('cv', ['slugifier'])
     .config(function($interpolateProvider){
