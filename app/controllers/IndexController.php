b8a73991664460b2b2244aa4e96125cf24757818
maxime <bertrand@nelis.fr>
2017-09-21 16:02:41 +0200

    replaced gulp with webpack

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -12,20 +12,22 @@ class IndexController extends Controller
         $this->assets->addJs("node_modules/angular-aria/angular-aria.min.js");
         $this->assets->addJs("node_modules/angular-slugify/angular-slugify.js");
 
+        $this->assets->addJs("dist/bundle.js");
+
         // Material design lite
         $this->assets->addCss("node_modules/material-design-lite/material.min.css");
         $this->assets->addJs("node_modules/material-design-lite/material.min.js");
 
         $this->assets->addCss("node_modules/mdi/css/materialdesignicons.min.css");
-        $this->assets->addCss("css/main.css");
+        // $this->assets->addCss("dist/css/main.css");
 
-        $this->assets->addJs("js/angular/app.js");
-        $this->assets->addJs("js/angular/controllers/index.js");
-        $this->assets->addJs("js/angular/controllers/skills.js");
-        $this->assets->addJs("js/angular/controllers/experience.js");
-        $this->assets->addJs("js/angular/controllers/training.js");
-        $this->assets->addJs("js/angular/controllers/projects.js");
-        $this->assets->addJs("js/angular/controllers/about.js");
-        $this->assets->addJs("js/angular/controllers/hobbies.js");
+        $this->assets->addJs("dist/js/angular/app.js");
+        $this->assets->addJs("dist/js/angular/controllers/index.js");
+        $this->assets->addJs("dist/js/angular/controllers/skills.js");
+        $this->assets->addJs("dist/js/angular/controllers/experience.js");
+        $this->assets->addJs("dist/js/angular/controllers/training.js");
+        $this->assets->addJs("dist/js/angular/controllers/projects.js");
+        $this->assets->addJs("dist/js/angular/controllers/about.js");
+        $this->assets->addJs("dist/js/angular/controllers/hobbies.js");
 	}
 }
